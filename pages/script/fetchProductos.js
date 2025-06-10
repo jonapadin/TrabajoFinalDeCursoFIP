658f8ba50082850a1e350a40087056d077c12499
cele_ruspil <celesteruspil22@gmail.com>
2025-06-10 16:05:57 -0300

    add actualizacion de script- productos

--- pages/script/fetchProductos.js
+++ pages/script/fetchProductos.js
@@ -1,4 +1,19 @@
-document.addEventListener( "DOMContentLoaded",function () {
+document.addEventListener( "DOMContentLoaded",async function () {
+
+    const contenedor=  document.getElementById("seccion-productos");
+    const card= document.createElement("DIV");
+    const imagenProducto = document.createElement("IMG");
+    const titulo = document.createElement("H2");
+    const descripcion= document.createElement("P");
+    const btnCompra = document.createElement("BUTTON");
+
+    contenedor.appendChild(card);
+    
+
+
+
+    
+    
     const url= "/productos.json";
     async function obtenerProductos () {
         try {
@@ -7,13 +22,15 @@ document.addEventListener( "DOMContentLoaded",function () {
                 throw new Error (`Error de status: ${response.status}`);
             }
             const data = await response.json();
-            console.log(data);
-            
+            return data;
         } catch (error) {
             console.error(error.message);
         }
-
         
-    }
-    obtenerProductos();
+        
+        
+    } 
+    const dataProduct = await obtenerProductos();
+      console.log(dataProduct);
+    
 })
