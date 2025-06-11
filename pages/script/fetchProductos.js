120de96192e9139226fd389531c6cfaac04a5320
cele_ruspil <celesteruspil22@gmail.com>
2025-06-11 18:01:57 -0300

    cargamos datos y le dimos estilos a los productos perros

--- pages/script/fetchProductos.js
+++ pages/script/fetchProductos.js
@@ -2,7 +2,16 @@
 document.addEventListener("DOMContentLoaded", async function () {
 
     const contenedor = document.getElementById("seccion-productos");
+    contenedor.className="seccionProductos" ;
 
+    const seccionAlimento = document.createElement("section");
+    seccionAlimento.className ="seccionAlimento";
+    
+    const titulo = document.createElement("H2");
+    titulo.textContent="Alimento";
+    titulo.className="titulo";
+    contenedor.appendChild(titulo);
+    
 
     // funcion para obtner productos
     const url = "/productos.json";
@@ -42,26 +51,36 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         productosPerro.forEach(producto => {
             const card = document.createElement("DIV");
+            card.className="card";
             const imagenProducto = document.createElement("IMG");
+            imagenProducto.className="imgProducto";
             imagenProducto.src=producto.imagen;
             imagenProducto.alt=`${producto.marca}`;
 
+            
             const titulo = document.createElement("H2");
-            titulo.textContent= producto.marca;
-
+            titulo.textContent= producto.marca;             
+            
             const descripcion = document.createElement("P");
             descripcion.textContent = producto.descripcion;
-
+            
             const btnCompra = document.createElement("BUTTON");
             btnCompra.textContent= `Comprar`;
-
+            btnCompra.className="btnCompra";
+            
+            const containerDesc = document.createElement("DIV");
+            containerDesc.className="containerDesc"; 
             // agrega datos a la card
             card.appendChild(imagenProducto);
-            card.appendChild(titulo);
-            card.appendChild(descripcion);
-            card.appendChild(btnCompra);
+            card.appendChild(containerDesc);
+            containerDesc.appendChild(titulo);
+            containerDesc.appendChild(descripcion);
+            containerDesc.appendChild(btnCompra);
+
             // agrega la cara del contenedor
-            contenedor.appendChild(card);
+            seccionAlimento.appendChild(card);
+            contenedor.appendChild(seccionAlimento);
+
 
 
         });
