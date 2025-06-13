29a4c5d51f9cfc7046c501397df75bffd1508648
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-06-13 19:15:37 -0300

    modularizacion de funciones y ajustes en html productos gatos

---
+++ pages/script/funciones.js
@@ -0,0 +1,41 @@
+
+//Crear tarjetas
+export function crearTarjeta(titulo, descripcion, infPago, precio, imagenSrc, altImg) {
+ const card = document.createElement("DIV");
+    card.className = "card";
+
+    const imagenProducto = document.createElement("IMG");
+    imagenProducto.className = "imgProducto";
+    imagenProducto.src = imagenSrc;
+    imagenProducto.alt = altImg;
+
+    const h2 = document.createElement("H2");
+    h2.textContent = titulo;
+
+    const h4 = document.createElement("H4");
+    h4.textContent = descripcion;
+
+    const p = document.createElement("P");
+    p.textContent = infPago;
+
+    const span = document.createElement("SPAN");
+    span.textContent = `$ ${precio}`;
+
+    const btnCompra = document.createElement("BUTTON");
+    btnCompra.textContent = "Comprar";
+    btnCompra.className = "btnCompra";
+
+    const containerDesc = document.createElement("DIV");
+    containerDesc.className = "containerDesc";
+    containerDesc.appendChild(h2);
+    containerDesc.appendChild(h4);
+    containerDesc.appendChild(p);
+    containerDesc.appendChild(span);
+
+    card.appendChild(imagenProducto);
+    card.appendChild(containerDesc);
+    card.appendChild(btnCompra);
+
+    return card;
+    
+}
--- pages/script/produGatos.js
+++ pages/script/produGatos.js
@@ -1,4 +1,5 @@
 import { obtenerProductos } from "./fetchProductos"; 
+import { crearTarjeta } from "./funciones";
 document.addEventListener("DOMContentLoaded", async function () {
 
     const contenedor = document.getElementById("seccion-productos");
@@ -22,60 +23,28 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 
     //acceder a la esctructura
-    const productosPerro = dataProduct[0]?.categoria?.Perro.Alimento;
+    const alimentos = dataProduct[0]?.categoria?.Gato.Alimento;
+    const accesorios = dataProduct[0]?.categoria?.Gato.Accesorios;
 
-    if (!productosPerro) {
-        console.log("No se encontro la categoria perros");
+    if (!alimentos) {
+        console.log("No se encontro la categoria Alimento");
         return
 
     }
 
     // iterar sobre alimentos y crear las cards
+alimentos.forEach(producto => {
+    const cardsAlimeto = crearTarjeta(
+        producto.marca,
+        producto.descripcion,
+        producto.opciones_pago.descripcion,
+        producto.precio,
+        producto.imagen,
+        producto.marca // para alt de imagen
+    );
 
-        productosPerro.forEach(producto => {
-            const card = document.createElement("DIV");
-            card.className="card";
-            const imagenProducto = document.createElement("IMG");
-            imagenProducto.className="imgProducto";
-            imagenProducto.src=producto.imagen;
-            imagenProducto.alt=`${producto.marca}`;
-
-            
-            const titulo = document.createElement("H2");
-            titulo.textContent= producto.marca;             
-            
-            const descripcion = document.createElement("H4");
-            descripcion.textContent = producto.descripcion;
-
-            const pagos= document.createElement("P");
-            pagos.textContent = `${producto.opciones_pago.descripcion}`;
-
-            const precio = document.createElement("SPAN");
-            precio.textContent= `$ ${producto.precio}`;
-
-            
-            const btnCompra = document.createElement("BUTTON");
-            btnCompra.textContent= `Comprar`;
-            btnCompra.className="btnCompra";
-            
-            const containerDesc = document.createElement("DIV");
-            containerDesc.className="containerDesc"; 
-            // agrega datos a la card
-            card.appendChild(imagenProducto);
-            card.appendChild(containerDesc);
-            containerDesc.appendChild(titulo);
-            containerDesc.appendChild(descripcion);
-            containerDesc.appendChild(pagos);
-            containerDesc.appendChild(precio);
-            card.appendChild(btnCompra);
-
-            // agrega la cara del contenedor
-            seccionAlimento.appendChild(card);
-            contenedor.appendChild(seccionAlimento);
-
-
-
-        });
-    
+    seccionAlimento.appendChild(cardsAlimeto);
+    contenedor.appendChild(seccionAlimento);
+});
 
 })
