0883f2354bd500940b359bc3dafd48a30245b336
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-06-19 16:04:10 -0300

    agrege menu desplegable, repare errores de navegacion, y agrege readme

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnMenu = document.getElementById("btn-menu");
+    const menu = document.getElementById("menu-desplegable");
 
+    btnMenu.addEventListener("click", () => {
+    const isHidden = menu.hasAttribute("hidden");
+    if (isHidden) {
+        menu.removeAttribute("hidden");
+    } else {
+        menu.setAttribute("hidden", "");
+    }
+    });
+})
