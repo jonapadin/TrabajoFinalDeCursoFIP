7c81e1d16170730ba51fa6a20599f4a0a4dbbd0e
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-06-21 13:47:12 -0300

    implementacion de registro en local storage

--- pages/script/registro.js
+++ pages/script/registro.js
@@ -1,3 +1,69 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const formularioRegistro = document.getElementById("formulario");
+
+    formularioRegistro.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const inputNombre = document.getElementById('nombre').value.trim();
+        const inputApellido = document.getElementById('apellido').value.trim();
+        const inputEdad = document.getElementById('edad').value.trim();
+        const inputClave = document.getElementById('pass').value.trim();
+        const inputReclave = document.getElementById('repass').value.trim();
+        const inputCorreo = document.getElementById('email').value.trim();
+        console.log(inputNombre, inputApellido, inputEdad, inputClave, inputReclave, inputCorreo);
+
+
+        if (inputClave !== inputReclave) {
+            console.log("las contraseñas deben coincidir")
+            return false;
+        }
+
+        function validar(obj) {
+            for (const [campo, valor] of Object.entries(obj)) {
+                if (!valor || valor.trim() === "") {
+                    console.log(`El campo "${campo}" no debe estar vacío.`);
+                    return false;
+                }
+
+                switch (campo) {
+                    case "nombre":
+                    case "apellido":
+                        if (valor.length < 3) {
+                            console.log(`El ${campo} debe contener un mínimo de 3 caracteres`);
+                            return false;
+                        }
+                        break;
+
+                }
+
+
+                if (campo === "email" && !/^\S+@\S+\.\S+$/.test(valor)) {
+                    console.log(`El campo "${campo}" debe ser un correo válido.`);
+                    return false;
+                }
+            }
+            console.log("Todos los campos son válidos.");
+            return true;
+        }
+
+        const usuario = {
+            nombre: inputNombre,
+            apellido: inputApellido,
+            edad: inputEdad,
+            pass: inputClave,
+            email: inputCorreo
+        }
+
+
+       if( validar(usuario)) {
+                    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+        usuarios.push(usuario);
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+       }
+
+       console.log("no hay datos para guardar")
+
+    })
 })
