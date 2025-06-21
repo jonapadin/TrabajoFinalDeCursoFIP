c7993f52d8058739029b146b716377542c08091e
cele_ruspil <celesteruspil22@gmail.com>
2025-06-20 21:53:16 -0300

    capturando email y redirigiendo a pagina + validaciones

---
+++ pages/script/login.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const formulario = document.getElementById("datosUsuario");
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const usuario = {
+            email: "Marina15@gmail.com",
+            contrasenia: "12154hola"
+        }
+
+        const email = document.getElementById("email").value.trim();
+        const pass = document.getElementById("password").value.trim();
+
+
+        if (usuario.email !== email || usuario.contrasenia !== pass) {
+            console.log("Usuario o contraseña incorrectos");
+            return;
+        }
+       
+        window.location.href = "http://localhost:5173/pages/usuario"
+        
+
+    });
+
+
+
+    
+});
